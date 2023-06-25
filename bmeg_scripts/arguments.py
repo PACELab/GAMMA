1b4e6f7ba7222c87b451881ba3e85823cf8807ff
gaganso <gagan.somashekar@gmail.com>
2023-06-25 00:08:33 +0000

    arguments

--- bmeg_scripts/arguments.py
+++ bmeg_scripts/arguments.py
@@ -3,6 +3,16 @@ import argparse
 def argument_parser():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        "--server_url", default="http://127.0.0.1:8000", help="The URL at which the SelfTune server is running"
+        "experiment_name", help="The name of the experiment"
     )
+    parser.add_argument("--rps", type=int, default = 800, help="The number of requests per second")
+    parser.add_argument(
+        "--config_file", help="The file containing all the configurations of the experiment."
+    )
+    parser.add_argument(
+        "--overwrite_experiment_folder",
+        action="store_true",
+        help="If set, overwrites the experiment results from a previous run.",
+    )
+
     return parser.parse_args()
