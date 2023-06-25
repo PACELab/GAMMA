0ee03dc47d478e3fb2075597a0d9a2e1eb59a39a
gaganso <gagan.somashekar@gmail.com>
2023-06-25 00:11:42 +0000

    folder to maintain scripts for anomaly generating tools

---
+++ cluster_maintanence/tools/install_cpu_load_generator_requirements.sh
@@ -0,0 +1,10 @@
+cmd="/home/ubuntu/firm_compass/tools/CPULoadGenerator/requirements.sh"
+for i in {2..16}
+do
+	echo userv$i
+	ssh -i ~/compass.key ubuntu@userv$i "$cmd"
+	echo "\n\n"
+
+done
+echo userv17-jaeger
+ssh -i ~/compass.key ubuntu@userv17-jaeger "$cmd"
