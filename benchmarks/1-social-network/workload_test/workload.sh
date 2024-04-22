6d4d43f9f00379206fecfeac9a59da41feeddd2e
gaganso <gagan.somashekar@gmail.com>
2024-04-22 22:54:08 +0000

    benchmark changes

--- benchmarks/1-social-network/workload_test/workload.sh
+++ benchmarks/1-social-network/workload_test/workload.sh
@@ -1,4 +1,4 @@
 front_end=$1
-/home/ubuntu/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P compose_latencies.txt -L -s /home/ubuntu/firm_compass/wrk2/scripts/social-network/compose-post.lua http://$front_end:8080/wrk2-api/post/compose -R 10 > compose.log
-/home/ubuntu/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P user_latencies.txt -L -s /home/ubuntu/firm_compass/wrk2/scripts/social-network/read-user-timeline.lua http://$front_end:8080/wrk2-api/user-timeline/read -R 30 > user.log
-/home/ubuntu/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P home_latencies.txt -L -s /home/ubuntu/firm_compass/wrk2/scripts/social-network/read-home-timeline.lua http://$front_end:8080/wrk2-api/home-timeline/read -R 60 > home.log
+/home/azureuser/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P compose_latencies.txt -L -s /home/azureuser/firm_compass/wrk2/scripts/social-network/compose-post.lua http://$front_end:31499/wrk2-api/post/compose -R 10 > compose.log
+/home/azureuser/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P user_latencies.txt -L -s /home/azureuser/firm_compass/wrk2/scripts/social-network/read-user-timeline.lua http://$front_end:31499/wrk2-api/user-timeline/read -R 30 > user.log
+/home/azureuser/firm_compass/wrk2/wrk -D exp -t 4 -c 8 -d 60 -P home_latencies.txt -L -s /home/azureuser/firm_compass/wrk2/scripts/social-network/read-home-timeline.lua http://$front_end:31499/wrk2-api/home-timeline/read -R 60 > home.log
